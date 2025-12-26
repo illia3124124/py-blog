6f7e4b21734f176d662e0cc6cd43cb0c47c0111d
Illia Mileckiy <illia.miletskyi@oa.edu.ua>
2025-12-26 16:47:43 +0200

    reformatted

--- blog/urls.py
+++ blog/urls.py
@@ -10,7 +10,11 @@ from blog.views import (
 urlpatterns = [
     path("", index, name="index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/commentaries/create", commentary_create, name="commentary-create"),
+    path(
+        "posts/<int:pk>/commentaries/create",
+        commentary_create,
+        name="commentary-create"
+    ),
 ]
 
 app_name = "blog"
